25081c7cc7ebfb690d90393b308773150599ba42
DHANANJAY-YADAV <daw123y@gmail.com>
2020-06-25 13:12:03 +0530

    Data Binding and directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,25 @@
 import {Component} from '@angular/core';
 @Component({
     selector:'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styles:[`
+    .online{
+        color:white;
+    }`]
 })
 export class ServerComponent
 {
-
+    serverId:number=10;
+    ServerStatus:string="offline";
+    constructor(){
+        this.ServerStatus=Math.random()>0.5?'online':'offline';
+    }
+    getServerStatus()
+    {
+        return this.ServerStatus;
+    }
+    getColor()
+    {
+        return this.ServerStatus=='offline'?'red':'green';
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
+  allowNewServer=false;
+  value="ADD Server";
+  serverName="abc";
+  servercreated=false;
+  CreateServerStatus="no server was created";
+  servers=["Testserver","testserver 2"];
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.allowNewServer=true;
+    }, 2000);
   }
-
+  onCreateServer()
+  {
+    this.CreateServerStatus="Server is created";
+    this.servercreated=!this.servercreated;
+    this.servers.push(this.serverName);
+  }
+  onUpdateServer(event: Event)
+  {
+    this.serverName=(<HTMLInputElement>event.target).value;
+  }
 }
